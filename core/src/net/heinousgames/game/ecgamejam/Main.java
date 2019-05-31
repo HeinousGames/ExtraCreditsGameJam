963bfca569210fdb97a3dc4730ddd1f0f8ff31fb
ministeve412 <ministeve412@gmail.com>
2019-05-30 23:32:20 -0700

    deleting initial project jpg and adding ceci's music

--- core/src/net/heinousgames/game/ecgamejam/Main.java
+++ core/src/net/heinousgames/game/ecgamejam/Main.java
@@ -3,34 +3,35 @@ package net.heinousgames.game.ecgamejam;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+
+import net.heinousgames.game.ecgamejam.screens.HeinousScreen;
 
 public class Main extends Game {
 
-	Preferences preferences;
-	SpriteBatch batch;
-	Texture img;
-	
+    public Music bgMusic;
+	public Preferences preferences;
+	public SpriteBatch batch;
+	public TmxMapLoader mapLoader;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        batch = new SpriteBatch();
+        mapLoader = new TmxMapLoader();
+        preferences = Gdx.app.getPreferences("ExtraCreditsGameJamSave");
+        setScreen(new HeinousScreen(this, "tiger.tmx"));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	    super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	    batch.dispose();
+	    bgMusic.dispose();
 	}
 }
