c1bbe376bddc253d0b547cd89faafc338b20f0fa
ministeve412 <ministeve412@gmail.com>
2019-05-30 23:31:43 -0700

    adding first mock level that can be abstracted further and assets

---
+++ android/assets/grey_wall_tileset_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grey_wall_tileset_32x32" tilewidth="32" tileheight="32" tilecount="15" columns="15">
+ <image source="grey_wall_tileset_32x32.png" width="480" height="32"/>
+</tileset>
---
+++ android/assets/tiger.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiger" tilewidth="1216" tileheight="1152" tilecount="1" columns="1">
+ <image source="tiger.png" width="1216" height="1152"/>
+</tileset>
