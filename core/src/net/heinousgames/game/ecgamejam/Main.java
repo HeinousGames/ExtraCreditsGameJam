de7c16d2cd44eae89dfeb1034f3211d892a67540
ministeve412 <ministeve412@gmail.com>
2019-06-01 17:34:12 -0700

    adding assets for in-game usage

--- core/src/net/heinousgames/game/ecgamejam/Main.java
+++ core/src/net/heinousgames/game/ecgamejam/Main.java
@@ -4,24 +4,34 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 
-import net.heinousgames.game.ecgamejam.screens.HeinousScreen;
+import net.heinousgames.game.ecgamejam.screens.StartScreen;
 
 public class Main extends Game {
 
     public Music bgMusic;
-	public Preferences preferences;
+	public Preferences prefs;
 	public SpriteBatch batch;
+	public Texture buttons, windows, bg;
 	public TmxMapLoader mapLoader;
 
 	@Override
 	public void create () {
         batch = new SpriteBatch();
         mapLoader = new TmxMapLoader();
-        preferences = Gdx.app.getPreferences("ExtraCreditsGameJamSave");
-        setScreen(new HeinousScreen(this, "drawbridge.tmx"));
+        prefs = Gdx.app.getPreferences("ExtraCreditsGameJamSave");
+        bgMusic = Gdx.audio.newMusic(Gdx.files.internal("Ceci Beats - Beach Daze.mp3"));
+        bgMusic.setLooping(true);
+//        bgMusic.play();
+
+        bg = new Texture("BG.png");
+        buttons = new Texture("Buttons.png");
+        windows = new Texture("Windows.png");
+
+        setScreen(new StartScreen(this));
 	}
 
 	@Override
@@ -33,5 +43,9 @@ public class Main extends Game {
 	public void dispose () {
 	    batch.dispose();
 	    bgMusic.dispose();
+
+	    bg.dispose();
+	    buttons.dispose();
+	    windows.dispose();
 	}
 }
